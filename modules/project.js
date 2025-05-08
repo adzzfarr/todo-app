315d5914a9cc0791dcc5a6c8f84d5714fa05b93c
Adzfar <53836459+adzzzfarr@users.noreply.github.com>
2025-05-08 19:27:46 +0800

    Created Task and Project factory functions

---
+++ modules/project.js
@@ -0,0 +1,28 @@
+function createProject(name) {
+    const tasks = [];
+
+    function orderTasks() {
+        tasks.sort((a, b) => a.priority - b.priority)
+    }
+
+    return { 
+        name,
+
+        getTasks() {
+            return tasks;
+        },
+
+        addTask(task) {
+            tasks.push(task);
+            orderTasks();
+        },
+
+        removeTask(taskName) {
+            const index = tasks.findIndex(task => task.name === taskName);
+            if (index != -1) {
+                tasks.splice(index, 1);
+            }
+            orderTasks();
+        },
+    }
+}
---
+++ modules/task.js
@@ -0,0 +1,12 @@
+function createTask(name, description, dueDate, priority) {
+    return {
+        name,
+        description,
+        dueDate,
+        priority,
+        completionStatus: false,
+        toggleCompletion() {
+            this.completionStatus = !this.completionStatus;
+        },
+    }
+}
